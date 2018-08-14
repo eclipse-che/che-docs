dd18567aae3b8bda30e087d5d1d3584b99dd36ee
Robert Krátký <rkratky@redhat.com>
2018-08-14 15:31:58 +0200

    Update the run script to use the new docs image.
    Signed-off-by: Robert Krátký <rkratky@redhat.com>

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run -ti -p 4000:4000 -v $(pwd)/src/main:/che-docs:Z eivantsov/jekyll-docs sh -c "cd /che-docs; mkdir -p _site; chown jekyll:jekyll _site; jekyll serve"
+docker run -ti -p 4000:4000 -v $(pwd)/src/main:/che-docs:Z rkratky/jekyll-docs sh -c "cd /che-docs; mkdir -p _site; chown jekyll:jekyll _site; jekyll serve"
 
