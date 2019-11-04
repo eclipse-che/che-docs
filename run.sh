75c0d2303cee4ca4a56a26ec177fa6b291ca2b87
Robert Krátký <rkratky@redhat.com>
2019-11-04 12:03:31 +0100

    Adding :Z for SELinux

--- run.sh
+++ run.sh
@@ -35,17 +35,17 @@ case "$1" in
     echo "Building for production (publishing on eclipse.org/che/docs/). To preview"
     echo "the docs locally, run the script ($0) without the '-prod' option."
     $RUNNER run --rm \
-      -v "${SRC_PATH}":/che-docs \
+      -v "${SRC_PATH}":/che-docs:Z \
       eclipse/che-docs \
       sh -c "cd /che-docs && jekyll clean && jekyll build --config _config.yml,_config-war.yml"
     ;;
   *)
-    echo "Building and serving the docs for local preview. To build for production"
+    echo "Building and serving the docs for local preview. To build for production testing"
     echo "(publishing on eclipse.org/che/docs/), run the script with the '-prod' option:"
     echo "$0 -prod"
     $RUNNER run --rm -ti \
       -p 35729:35729 -p 4000:4000 \
-      -v "${SRC_PATH}":/che-docs \
+      -v "${SRC_PATH}":/che-docs:Z \
       eclipse/che-docs \
       sh -c "cd /che-docs && jekyll clean && jekyll serve --livereload -H 0.0.0.0 --trace"
   ;;
