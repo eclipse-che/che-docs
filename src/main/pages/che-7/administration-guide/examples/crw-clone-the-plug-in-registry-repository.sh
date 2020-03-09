e47d4aed7fc795fe60777f6d790f41b84e641da8
Michal Maléř <48474054+MichalMaler@users.noreply.github.com>
2020-03-09 10:42:44 +0100

    Cleaning attributes and example files (#1112)
    Signed-off-by: Michal Maléř <mmaler@redhat.com>

--- src/main/pages/che-7/administration-guide/examples/crw-clone-the-plug-in-registry-repository.sh
+++
@@ -1,7 +0,0 @@
-$ podman login registry.redhat.io
-Username: ${REGISTRY-SERVICE-ACCOUNT-USERNAME}
-Password: ${REGISTRY-SERVICE-ACCOUNT-PASSWORD}
-Login Succeeded!
-
-$ podman pull registry.redhat.io/codeready-workspaces/pluginregistry-rhel8
-$ cd codeready-workspaces/pluginregistry-rhel8
