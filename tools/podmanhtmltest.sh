2e9a71483177d3b8e1f6b00a04f1526a6a2baa72
Fabrice Flore-Thébault <ffloreth@redhat.com>
2021-01-26 11:54:20 +0100

    Add license, run also on docker

--- tools/podmanhtmltest.sh
+++ tools/podmanhtmltest.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env sh
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
 set -ex
 
-podman run --rm -ti \
+echo "Run htmltest in a container"
+
+if command -v podman 
+then
+  RUNNER=podman
+elif command -v docker 
+then 
+  RUNNER=docker
+else
+  echo "podman or docker commands not available. Abandoning."
+  exit 1
+fi
+
+${RUNNER} run --rm -ti \
   -v "$(pwd):/test:Z" \
-  "${CHE_DOCS_IMAGE:-wjdp/htmltest}" \
+  "wjdp/htmltest" \
   -c .htmltest.yml
