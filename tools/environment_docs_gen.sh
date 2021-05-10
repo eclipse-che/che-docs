c7a6f96910c818633a52e1761dd86fe380c7d648
nickboldt <nboldt@redhat.com>
2021-05-10 09:15:54 -0300

    instead of sed to remove spaces, use tr to remove spaces AND quotes
    Change-Id: I5384aa9a3bc25e5423ab0efb719e6b3e839a4fb7
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- tools/environment_docs_gen.sh
+++ tools/environment_docs_gen.sh
@@ -22,7 +22,7 @@ OUTPUT_PATH="$PARENT_PATH/modules/installation-guide/examples/system-variables.a
 fetch_current_version() {
   echo "Trying to read current product version from $PARENT_PATH/antora-playbook.yml..." >&2
   
-  CURRENT_VERSION=$(grep 'prod-ver:' "$PARENT_PATH/antora-playbook.yml" | cut -d: -f2 | sed  's/ //g').x
+  CURRENT_VERSION=$(grep 'prod-ver:' "$PARENT_PATH/antora-playbook.yml" | cut -d: -f2 | tr -d ' "').x
   if [ $? -ne 0 ]; then
     echo "Failure: Cannot read version from $PARENT_PATH/antora-playbook.yml" >&2
     exit 1
