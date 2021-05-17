ecdbda3fafcafdde217963848c49b24864ea81f2
Fabrice Flore-Thébault <ffloreth@redhat.com>
2021-05-17 13:45:13 +0200

    Ignore vale on checluster-properties.adoc

--- tools/environment_docs_gen.sh
+++ tools/environment_docs_gen.sh
@@ -91,6 +91,9 @@ parse_content() {
       DESCR_BUFF="$(sed -E 's|https://www.keycloak.org/docs/3.3/server_admin/topics/identity-broker/social/openshift.html|https://www.keycloak.org/docs/latest/server_admin/index.html#openshift-4|' <<< $DESCR_BUFF)"   # Fix broken link
       DESCR_BUFF="$(sed -E 's|k8s|{orch-name}|g' <<< $DESCR_BUFF)"   # k8s to {orch-name}
       DESCR_BUFF="$(sed -E 's| openshift| {ocp}|g' <<< $DESCR_BUFF)"   # k8s to {orch-name}
+      DESCR_BUFF="$(sed -E 's|che-host|prod-host|g' <<< $DESCR_BUFF)"   # fix missing attribute che-host
+      DESCR_BUFF="$(sed -E 's|\{WORKSPACE_ID|\\\{WORKSPACE_ID|g' <<< $DESCR_BUFF)"   # fix missing attribute WORKSPACE_ID
+      DESCR_BUFF="$(sed -E 's|\{generated_8_chars|\\\{generated_8_chars|g' <<< $DESCR_BUFF)"   # fix missing attribute generated_8_chars
 
       DESCR_BUFF="${DESCR_BUFF/ }"                      # trim first space
       BUFF="$BUFF $ENV,\"$VALUE\",\"${DESCR_BUFF//\"/\'}\" $NEWLINE"   # apply key value and description buffer
