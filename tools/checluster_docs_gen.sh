0dac85558ab989c22ef711f0d7b6226f26c08820
Mykhailo Kuznietsov <mkuznets@redhat.com>
2021-08-04 17:59:17 +0300

    Update checluster_docs_gen.sh

--- tools/checluster_docs_gen.sh
+++ tools/checluster_docs_gen.sh
@@ -41,7 +41,7 @@ fetch_conf_files_content() {
   echo "Fetching property files content from GitHub..." >&2
 
   if [[ $PRODUCT == "che" ]]; then
-    CHECLUSTER_PROPERTIES_URL=https://raw.githubusercontent.com/eclipse-che/che-operator/$CURRENT_VERSION/config/crd/bases/org_v1_che_crd.yaml"
+    CHECLUSTER_PROPERTIES_URL="https://raw.githubusercontent.com/eclipse-che/che-operator/$CURRENT_VERSION/config/crd/bases/org_v1_che_crd.yaml"
   else
     CHECLUSTER_PROPERTIES_URL="https://raw.githubusercontent.com/redhat-developer/codeready-workspaces-operator/$CURRENT_VERSION/config/crd/bases/org_v1_che_crd.yaml"
   fi
