90b518675a8ceb123418bb1e79537e0b79e787b2
Fabrice Flore-Thébault <ffloreth@redhat.com>
2022-06-21 09:43:12 +0200

    chore: get_vale_styles compatible with unzip rtaher than busybox unzip

--- tools/get_vale_styles.sh
+++ tools/get_vale_styles.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # Copyright (c) 2021 Red Hat, Inc.
 # This program and the accompanying materials are made
@@ -7,12 +7,23 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
+# Download a fresh version of the RedHat and CheDocs Vale styles.
 
 # Fail on errors
 set -e
-
-# Get fresh Vale styles
-cd .vale/styles || exit
-rm -rf RedHat CheDocs 
-wget -qO- https://github.com/redhat-documentation/vale-at-red-hat/releases/latest/download/RedHat.zip | unzip -q -
-wget -qO- https://github.com/eclipse-che/che-docs-vale-style/releases/latest/download/CheDocs.zip | unzip -q -
+# New files will be group writeable (for local builds)
+umask 002
+ScriptDir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+RootDir="${ScriptDir%/tools*}"
+StylesDir="${RootDir}/.github/styles"
+# Create destination directory if required
+mkdir --parents "${StylesDir}"
+# Download a fresh version of the RedHat Vale style
+wget --output-document="${StylesDir}/RedHat.zip" --quiet --timestamping  https://github.com/redhat-documentation/vale-at-red-hat/releases/latest/download/RedHat.zip
+rm --force --recursive "${StylesDir}/RedHat"
+unzip -q -d  "${StylesDir}" RedHat.zip
+# Download a fresh version of the CheDocs Vale style
+wget --output-document="${StylesDir}/CheDocs.zip" --quiet --timestamping https://github.com/eclipse-che/che-docs-vale-style/releases/latest/download/CheDocs.zip
+rm --force --recursive "${StylesDir}/CheDocs"
+unzip -q -d  "${StylesDir}" CheDocs.zip
+echo 'INFO: Downloaded a fresh version of the Red Hat Vale style and configuration file.'
