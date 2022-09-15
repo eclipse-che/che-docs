d7d4fd85cc42644ac40a4e41bd073b7437c8880f
Anatolii Bazko <abazko@redhat.com>
2022-09-15 12:35:18 +0300

    feat: Adopt rollback.sh script to work after any migration steps
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- modules/administration-guide/attachments/migration/rollback.sh
+++ modules/administration-guide/attachments/migration/rollback.sh
@@ -20,45 +20,43 @@ PRE_MIGRATION_PRODUCT_OLM_CHANNEL=${PRE_MIGRATION_PRODUCT_OLM_CHANNEL:-stable}  
 PRE_MIGRATION_PRODUCT_OLM_CATALOG_SOURCE=${PRE_MIGRATION_PRODUCT_OLM_CATALOG_SOURCE:-community-operators} # {pre-migration-prod-catalog-source}
 PRE_MIGRATION_PRODUCT_OLM_STARTING_CSV=${PRE_MIGRATION_PRODUCT_OLM_STARTING_CSV:-eclipse-che.v7.41.2}     # {pre-migration-prod-starting-csv}
 
-DB_DUMP="${PRODUCT_ID}"-original-db.sql
-CHE_CLUSTER="${PRODUCT_ID}"-original-checluster.json
+ORIGINAL_DB_DUMP="${PRODUCT_ID}"-original-db.sql
+ORIGINAL_CHE_CLUSTER_DUMP="${PRODUCT_ID}"-original-checluster.json
 
 echo "[INFO] Retrieving ${PRODUCT_ID} database name."
 CHE_POSTGRES_DB=$("${K8S_CLI}" get cm/che -n "${INSTALLATION_NAMESPACE}" -o jsonpath='{.data.CHE_JDBC_URL}' | awk -F '/' '{print $NF}')
 
-deleteOperatorCSV() {
-    if "${K8S_CLI}" get subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}" > /dev/null 2>&1 ; then
-        echo "[INFO] Deleting operator cluster service version."
-        "${K8S_CLI}" delete csv "$("${K8S_CLI}" get subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}" -o jsonpath="{.status.installedCSV}")" -n "${OPERATOR_NAMESPACE}"
-    else
-        echo "[INFO] Skipping CSV deletion. No ${PRODUCT_ID} operator subscription found."
-    fi
+deleteProductOperatorCSVIfExists() {
+  if "${K8S_CLI}" get subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}" > /dev/null 2>&1 ; then
+    echo "[INFO] Deleting operator cluster service version."
+    "${K8S_CLI}" delete csv "$("${K8S_CLI}" get subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}" -o jsonpath="{.status.installedCSV}")" -n "${OPERATOR_NAMESPACE}"
+  else
+    echo "[INFO] Skipping CSV deletion. No ${PRODUCT_ID} operator subscription found."
+  fi
 }
 
-deleteOperatorSubscription() {
-    if "${K8S_CLI}" get subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}" > /dev/null 2>&1 ; then
-        echo "[INFO] Deleting ${PRODUCT_ID} operator subscription."
-        "${K8S_CLI}" delete subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}"
+deleteProductOperatorSubscriptionIfExists() {
+  if "${K8S_CLI}" get subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}" > /dev/null 2>&1 ; then
+    echo "[INFO] Deleting ${PRODUCT_ID} operator subscription."
+    "${K8S_CLI}" delete subscription "${PRODUCT_ID}" -n "${OPERATOR_NAMESPACE}"
 
-        echo "[INFO] Waiting 30s for the old ${PRODUCT_ID} operator deletion."
-        sleep 30
-    else
-        echo "[INFO] Skipping subscription deletion as no ${PRODUCT_ID} operator subscription was found."
-    fi
+    echo "[INFO] Waiting 30s for the old ${PRODUCT_ID} operator deletion."
+    sleep 30
+  else
+    echo "[INFO] Skipping subscription deletion as no ${PRODUCT_ID} operator subscription was found."
+  fi
 }
 
-patchCheCluster() {
-    if [[ -f "${CHE_CLUSTER}" ]]; then
-      echo "[INFO] Updating ${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME} CheCluster CR to set DevWorkspace engine mode."
-      DEV_WORKSPACE_ENABLE=$(cat "${CHE_CLUSTER}" | jq -r '.spec.devWorkspace.enable')
-      "${K8S_CLI}" patch checluster/"${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME}" -n "${INSTALLATION_NAMESPACE}" --type=json -p \
-          '[{"op": "replace", "path": "/spec/devWorkspace/enable", "value": '${DEV_WORKSPACE_ENABLE}'}]'
+restoreCheCluster() {
+  echo "[INFO] Updating ${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME} CheCluster CR to set DevWorkspace engine mode."
+  DEV_WORKSPACE_ENABLE=$(cat "${ORIGINAL_CHE_CLUSTER_DUMP}" | jq -r '.spec.devWorkspace.enable')
+  "${K8S_CLI}" patch checluster/"${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME}" -n "${INSTALLATION_NAMESPACE}" --type=json -p \
+      '[{"op": "replace", "path": "/spec/devWorkspace/enable", "value": '${DEV_WORKSPACE_ENABLE}'}]'
 
-      echo "[INFO] Updating ${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME} CheCluster CR to set server exposure strategy."
-      SEVER_EXPOSURE_STRATEGY=$(cat "${CHE_CLUSTER}" | jq -r '.spec.server.serverExposureStrategy')
-      "${K8S_CLI}" patch checluster/"${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME}" -n "${INSTALLATION_NAMESPACE}" --type=json -p \
-        '[{"op": "replace", "path": "/spec/server/serverExposureStrategy", "value": "'${SEVER_EXPOSURE_STRATEGY}'"}]'
-    fi
+  echo "[INFO] Updating ${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME} CheCluster CR to set server exposure strategy."
+  SEVER_EXPOSURE_STRATEGY=$(cat "${ORIGINAL_CHE_CLUSTER_DUMP}" | jq -r '.spec.server.serverExposureStrategy')
+  "${K8S_CLI}" patch checluster/"${PRE_MIGRATION_PRODUCT_CHE_CLUSTER_CR_NAME}" -n "${INSTALLATION_NAMESPACE}" --type=json -p \
+    '[{"op": "replace", "path": "/spec/server/serverExposureStrategy", "value": "'${SEVER_EXPOSURE_STRATEGY}'"}]'
 }
 
 terminatePostgresConnections() {
@@ -77,41 +75,41 @@ createDatabase() {
 }
 
 restoreDatabase() {
-  echo "[INFO] Uploading ${DB_DUMP} to the PostgreSQL pod."
-  "${K8S_CLI}" cp "${DB_DUMP}" "${INSTALLATION_NAMESPACE}"/"$("${K8S_CLI}" get pods -l app.kubernetes.io/component=postgres -n "${INSTALLATION_NAMESPACE}" --no-headers=true  -o custom-columns=":metadata.name")":/tmp/che.sql
+  echo "[INFO] Uploading ${ORIGINAL_DB_DUMP} to the PostgreSQL pod."
+  "${K8S_CLI}" cp "${ORIGINAL_DB_DUMP}" "${INSTALLATION_NAMESPACE}"/"$("${K8S_CLI}" get pods -l app.kubernetes.io/component=postgres -n "${INSTALLATION_NAMESPACE}" --no-headers=true  -o custom-columns=":metadata.name")":/tmp/che.sql
 
-  echo "[INFO] Populating ${DB_DUMP} database with the new content."
+  echo "[INFO] Populating ${ORIGINAL_DB_DUMP} database with the new content."
   "${K8S_CLI}" exec deploy/postgres -n "${INSTALLATION_NAMESPACE}"  -- bash  -c "psql ${CHE_POSTGRES_DB} < /tmp/che.sql"
 }
 
-deleteObjects() {
-    echo "[INFO] Deleting resources."
+deleteProductResourcesIfExist() {
+  echo "[INFO] Deleting resources."
 
-    echo "[INFO] Deleting deployments."
-    for DEPLOYMENT in "${PRODUCT_DEPLOYMENT_NAME}" "${PRODUCT_SHORT_ID}-dashboard" "plugin-registry" "devfile-registry" "che-gateway" "postgres"; do
-        "${K8S_CLI}" delete deployment "${DEPLOYMENT}" -n "${INSTALLATION_NAMESPACE}" > /dev/null 2>&1 || echo "[INFO] deployment ${DEPLOYMENT} not found."
-    done  
+  echo "[INFO] Deleting deployments."
+  for DEPLOYMENT in "${PRODUCT_DEPLOYMENT_NAME}" "${PRODUCT_SHORT_ID}-dashboard" "plugin-registry" "devfile-registry" "che-gateway" "postgres"; do
+    "${K8S_CLI}" delete deployment "${DEPLOYMENT}" -n "${INSTALLATION_NAMESPACE}" > /dev/null 2>&1 || echo "[INFO] deployment ${DEPLOYMENT} not found."
+  done
 
-    echo "[INFO] Deleting routes."
-    "${K8S_CLI}" delete route "${PRODUCT_SHORT_ID}" -n "${INSTALLATION_NAMESPACE}" > /dev/null 2>&1 || echo "[INFO] Route ${PRODUCT_SHORT_ID} not found."
+  echo "[INFO] Deleting routes."
+  "${K8S_CLI}" delete route "${PRODUCT_SHORT_ID}" -n "${INSTALLATION_NAMESPACE}" > /dev/null 2>&1 || echo "[INFO] Route ${PRODUCT_SHORT_ID} not found."
 
-    echo "[INFO] Deleting services."
-    for SERVICE in "che-host" "${PRODUCT_SHORT_ID}-dashboard" "plugin-registry" "devfile-registry" "che-gateway" "postgres"; do
-        "${K8S_CLI}" delete service "${SERVICE}" -n "${INSTALLATION_NAMESPACE}" > /dev/null 2>&1 || echo "[INFO] Service ${SERVICE} not found."
-    done
+  echo "[INFO] Deleting services."
+  for SERVICE in "che-host" "${PRODUCT_SHORT_ID}-dashboard" "plugin-registry" "devfile-registry" "che-gateway" "postgres"; do
+    "${K8S_CLI}" delete service "${SERVICE}" -n "${INSTALLATION_NAMESPACE}" > /dev/null 2>&1 || echo "[INFO] Service ${SERVICE} not found."
+  done
 
-    echo "[INFO] Deleting console link."
-    "${K8S_CLI}" delete consolelink che > /dev/null 2>&1 || echo "[INFO] ConsoleLink che not found."
+  echo "[INFO] Deleting console link."
+  "${K8S_CLI}" delete consolelink che > /dev/null 2>&1 || echo "[INFO] ConsoleLink che not found."
 }
 
-createOperatorSubscription() {
-    echo "[INFO] Creating new ${PRE_MIGRATION_PRODUCT_SUBSCRIPTION_NAME} operator subscription." 
-    echo "[INFO] New subscription name: ${PRE_MIGRATION_PRODUCT_OLM_PACKAGE}."
-    echo "[INFO] New subscription channel: ${PRE_MIGRATION_PRODUCT_OLM_CHANNEL}."
-    echo "[INFO] New subscription source: ${PRE_MIGRATION_PRODUCT_OLM_CATALOG_SOURCE}."
-    echo "[INFO] New subscription starting CSV: ${PRE_MIGRATION_PRODUCT_OLM_STARTING_CSV}."
+createPreMigrationProductOperatorSubscription() {
+  echo "[INFO] Creating new ${PRE_MIGRATION_PRODUCT_SUBSCRIPTION_NAME} operator subscription."
+  echo "[INFO] New subscription name: ${PRE_MIGRATION_PRODUCT_OLM_PACKAGE}."
+  echo "[INFO] New subscription channel: ${PRE_MIGRATION_PRODUCT_OLM_CHANNEL}."
+  echo "[INFO] New subscription source: ${PRE_MIGRATION_PRODUCT_OLM_CATALOG_SOURCE}."
+  echo "[INFO] New subscription starting CSV: ${PRE_MIGRATION_PRODUCT_OLM_STARTING_CSV}."
 
-    "${K8S_CLI}" apply -f - <<EOF
+  "${K8S_CLI}" apply -f - <<EOF
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
@@ -135,6 +133,16 @@ EOF
   "${K8S_CLI}" patch installplan ${INSTALL_PLAN} -n "${PRE_MIGRATION_PRODUCT_OPERATOR_NAMESPACE}" --type=merge -p '{"spec": {"approved": true}}'
 }
 
+scaleUpCheServer() {
+  echo "[INFO] Scaling up ${PRE_MIGRATION_PRODUCT_DEPLOYMENT_NAME}"
+  "${K8S_CLI}" scale deployment "${PRE_MIGRATION_PRODUCT_DEPLOYMENT_NAME}" --replicas=1 -n "${INSTALLATION_NAMESPACE}"
+}
+
+scaleUpKeycloak() {
+  echo "[INFO] Scaling up ${PRE_MIGRATION_PRODUCT_IDENTITY_PROVIDER_DEPLOYMENT_NAME}"
+  "${K8S_CLI}" scale deployment "${PRE_MIGRATION_PRODUCT_IDENTITY_PROVIDER_DEPLOYMENT_NAME}" --replicas=1 -n "${INSTALLATION_NAMESPACE}"
+}
+
 waitForComponent() {
   component=$1
   namespace=$2
@@ -148,21 +156,32 @@ waitForComponent() {
   "${K8S_CLI}" wait --for=condition=ready pod -l app.kubernetes.io/component="${component}" -n "${namespace}" --timeout=120s
 }
 
-deleteOperatorCSV
-deleteOperatorSubscription
-patchCheCluster
+deleteProductOperatorCSVIfExists
+deleteProductOperatorSubscriptionIfExists
 
-if [[ -f "${DB_DUMP}" ]]; then
+if [[ -f "${ORIGINAL_DB_DUMP}" ]]; then
   terminatePostgresConnections
   dropExistingDatabase
   createDatabase
   restoreDatabase
 fi
 
-deleteObjects
-createOperatorSubscription
+if [[ -f "${ORIGINAL_CHE_CLUSTER_DUMP}" ]]; then
+  restoreCheCluster
+fi
 
-waitForComponent "${PRE_MIGRATION_PRODUCT_OPERATOR_NAME}" "${PRE_MIGRATION_PRODUCT_OPERATOR_NAMESPACE}"
-waitForComponent "${PRE_MIGRATION_PRODUCT_DEPLOYMENT_NAME}" "${INSTALLATION_NAMESPACE}"
+if "${K8S_CLI}" get subscription "${PRE_MIGRATION_PRODUCT_SUBSCRIPTION_NAME}" -n "${PRE_MIGRATION_PRODUCT_OPERATOR_NAMESPACE}" > /dev/null 2>&1 ; then
+  # rollback is run after 1 or 2 migration steps.
+  scaleUpKeycloak
+  waitForComponent "${PRE_MIGRATION_PRODUCT_IDENTITY_PROVIDER_DEPLOYMENT_NAME}" "${INSTALLATION_NAMESPACE}"
+  scaleUpCheServer
+  waitForComponent "${PRE_MIGRATION_PRODUCT_DEPLOYMENT_NAME}" "${INSTALLATION_NAMESPACE}"
+else
+  # rollback is run after 3 or 4 migration steps.
+  deleteProductResourcesIfExist
+  createPreMigrationProductOperatorSubscription
+  waitForComponent "${PRE_MIGRATION_PRODUCT_OPERATOR_NAME}" "${PRE_MIGRATION_PRODUCT_OPERATOR_NAMESPACE}"
+  waitForComponent "${PRE_MIGRATION_PRODUCT_DEPLOYMENT_NAME}" "${INSTALLATION_NAMESPACE}"
+fi
 
 echo "[INFO] Done."
