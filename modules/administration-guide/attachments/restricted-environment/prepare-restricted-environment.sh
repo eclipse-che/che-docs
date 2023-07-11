b9951542fc7072e51bdc7e27ced04b8c645705ea
Anatolii Bazko <abazko@redhat.com>
2023-07-11 10:32:08 +0200

    fixes
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- modules/administration-guide/attachments/restricted-environment/prepare-restricted-environment.sh
+++ modules/administration-guide/attachments/restricted-environment/prepare-restricted-environment.sh
@@ -42,7 +42,7 @@ declare k8s_resource_name=${my_catalog}
 # Create local directories
 mkdir -p "${my_catalog}/${devworkspace_operator_package_name}" "${my_catalog}/${prod_operator_package_name}"
 
-echo "Fetching metadata for the ${devworkspace_operator_package_name} operator catalog channel, packages, and bundles."
+echo "[INFO] Fetching metadata for the ${devworkspace_operator_package_name} operator catalog channel, packages, and bundles."
 opm render "${devworkspace_operator_index}" \
   | jq "select \
     (\
@@ -55,7 +55,7 @@ opm render "${devworkspace_operator_index}" \
       |= [{name: \"${devworkspace_operator_package_name}.${devworkspace_operator_version}\"}]" \
   > "${my_catalog}/${devworkspace_operator_package_name}/render.json"
 
-echo "Fetching metadata for the ${prod_operator_package_name} operator catalog channel, packages, and bundles."
+echo "[INFO] Fetching metadata for the ${prod_operator_package_name} operator catalog channel, packages, and bundles."
 opm render "${prod_operator_index}" \
   | jq "select \
     (\
@@ -68,20 +68,20 @@ opm render "${prod_operator_index}" \
       |= [{name: \"${prod_operator_package_name}.${prod_operator_version}\"}]" \
   > "${my_catalog}/${prod_operator_package_name}/render.json"
 
-echo "Creating the catalog dockerfile."
+echo "[INFO] Creating the catalog dockerfile."
 if [ -f "${my_catalog}.Dockerfile" ]; then
   rm -f "${my_catalog}.Dockerfile"
 fi
 opm alpha generate dockerfile "./${my_catalog}"
 
-echo "Building the catalog image locally."
+echo "[INFO] Building the catalog image locally."
 podman build -t "${my_operator_index}" -f "./${my_catalog}.Dockerfile" --no-cache .
 
-echo "Disabling the default Red Hat Ecosystem Catalog."
+echo "[INFO] Disabling the default Red Hat Ecosystem Catalog."
 oc patch OperatorHub cluster --type json \
     --patch '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
 
-echo "Deploying your catalog image to the $my_operator_index registry."
+echo "[INFO] Deploying your catalog image to the $my_operator_index registry."
 # See: https://docs.openshift.com/container-platform/latest/installing/disconnected_install/installing-mirroring-installation-images.html#olm-mirroring-catalog_installing-mirroring-installation-images
 skopeo copy --dest-tls-verify=false --all "containers-storage:$my_operator_index" "docker://$my_operator_index"
 
@@ -95,24 +95,24 @@ spec:
       openVSXURL: ""
 EOF
 
-echo "Removing index image from mappings.txt to prepare mirroring."
+echo "[INFO] Removing index image from mappings.txt to prepare mirroring."
 oc adm catalog mirror "$my_operator_index" "$my_registry" --insecure --manifests-only | tee catalog_mirror.log
 MANIFESTS_FOLDER=$(sed -n -e 's/^wrote mirroring manifests to \(.*\)$/\1/p' catalog_mirror.log |xargs) # The xargs here is to trim whitespaces
 sed -i -e "/${my_operator_index_image_name_and_tag}/d" "${MANIFESTS_FOLDER}/mapping.txt"
 cat "${MANIFESTS_FOLDER}/mapping.txt"
 
-echo "Mirroring related images to the $my_registry registry."
+echo "[INFO] Mirroring related images to the $my_registry registry."
 # oc image mirror --insecure=true -f "${MANIFESTS_FOLDER}/mapping.txt"
 while IFS= read -r line
 do
   public_image=$(echo "${line}" | cut -d '=' -f1)
   private_image=$(echo "${line}" | cut -d '=' -f2)
-  echo "Mirroring ${public_image}"
+  echo "[INFO] Mirroring ${public_image}"
   skopeo copy --dest-tls-verify=false --preserve-digests --all "docker://$public_image" "docker://$private_image"
 done < "${MANIFESTS_FOLDER}/mapping.txt"
 
-echo "Creating CatalogSource and ImageContentSourcePolicy"
+echo "[INFO] Creating CatalogSource and ImageContentSourcePolicy"
 cat ${MANIFESTS_FOLDER}/catalogSource.yaml | sed 's|name: .*|name: '${k8s_resource_name}'|' | oc apply -f -
 cat ${MANIFESTS_FOLDER}/imageContentSourcePolicy.yaml | sed 's|name: .*|name: '${k8s_resource_name}'|' | oc apply -f -
 
-echo "INFO: Catalog $my_operator_index deployed to the $my_registry registry."
+echo "[INFO] Catalog $my_operator_index deployed to the $my_registry registry."
