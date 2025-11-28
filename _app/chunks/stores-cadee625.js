c3541cd6521bfbfbdb58a3292eb84c475ea3cb65
mkuznyetsov <mkuznyetsov@users.noreply.github.com>
2025-11-28 16:17:13 +0000

    deploy: 179e011ea685a41a2d4f4f7d60306b6a3fec0518

---
+++ _app/chunks/stores-cadee625.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index-5a101031.js";const o=e(!1),s=e("Home"),t=e("Run your favorite IDE on Kubernetes"),r=e("");export{r as a,t as b,o as d,s as p};
---
+++ _app/chunks/variables-007bace1.js
@@ -0,0 +1,1 @@
+let e;e="/che";const s=`${e}/images-website`,t={isDevelopment:!1,basePath:e,imagesPath:s,githubRepo:"eclipse/che"};export{t as v};
